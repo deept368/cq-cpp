b30ed7ae8ac06ee042393a8e79a02265f5465288
Ajit Jadhav <ajit.171198@gmail.com>
2023-03-13 21:23:17 -0700

    Create query mapping class

---
+++ src/cq/querymapping.cc
@@ -0,0 +1,52 @@
+#include "querymapping.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "../config.h"
+
+using namespace std;
+
+namespace lh
+{
+
+    QueryMapping::QueryMapping(){
+        readQueryMapping(QUERY_FILE);
+    }
+
+    QueryMapping::~QueryMapping(){
+    }   
+    
+   
+
+    void QueryMapping::readQueryMapping(string queryFile){
+        ifstream file(queryFile);
+        if (!file) {
+            cerr << "Error opening query file: " << queryFile << endl;
+            return;
+        }
+
+        string line;
+        while (getline(file, line)) {
+            istringstream ss(line);
+            string queryId, query;
+            ss >> queryId;
+            
+            // Extract substring starting from first non-tab character
+            int start = line.find_first_not_of('\t', queryId.length());
+            query = line.substr(start);
+            
+           
+            queryMapping.insert(make_pair(stoi(queryId), query));
+        }
+        file.close();
+        cout << "Loading query mapping completed." << endl;
+    }
+
+    string QueryMapping::getQuery(int queryId){
+        if (queryMapping.find(queryId) == queryMapping.end()) {
+            cerr << "Query ID " << queryId << " not found." << endl;
+            return "";
+        }
+        return queryMapping[queryId];
+    }
+};
--- src/cq/queryprocessor.h
+++ src/cq/queryprocessor.h
@@ -15,19 +15,15 @@ namespace lh{
     class QueryProcessor
     {
     private:
-        unordered_map<int, string> queryMapping;
         unordered_map<int, vector<string>> queryResults;
         CodeFetcher* code_fetcher;
 
-        void readQueryMapping(string queryFile);
         void readQueryResults(string resultFile);
     
     public:
         QueryProcessor();
         ~QueryProcessor();
         unordered_map<int, unordered_map<string, vector<vector<int>>>> getCodes();
-        string getQuery(int queryId);
-        // void print_doc_data(unordered_map<string, vector<vector<int>>> doc_data_map);
        
     };
 }
