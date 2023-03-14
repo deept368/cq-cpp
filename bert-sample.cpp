d46fe03cd3e8a022286c83eba07caf6076806e57
Deept Mahendiratta <dipt1995@gmail.com>
2023-03-13 20:29:04 -0700

    big fix

--- bert-sample.cpp
+++ bert-sample.cpp
@@ -7,6 +7,16 @@ using namespace lh;
 #include<unordered_map>
 
 int main(){      
+
+    #ifdef PRFILE_CQ
+        auto begin = std::chrono::system_clock::now();
+    #endif
+
     ExecuteCQ cq;
     cq.execute();
+
+    #ifdef PRFILE_CQ
+        auto end = std::chrono::system_clock::now();
+        std::cout<<"total execution time including loading in milli-seconds "<< (std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count())/1000 << std::endl;
+    #endif
 }
